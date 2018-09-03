2d824fd50e62b5d1e4e0962be42090dea487c995
Andrew Boro <42089617+andrewboro@users.noreply.github.com>
2018-09-03 11:54:40 +0200

    insert node into tree

--- src/com/example/exercises/binarysearchtree/Node.java
+++ src/com/example/exercises/binarysearchtree/Node.java
@@ -1,11 +1,16 @@
 package com.example.exercises.binarysearchtree;
 
 public class Node<T extends Comparable<T>> {
-	
 	T value;
 	Node<T> left;
-	Node<T>	right;
-	
-	
+	Node<T> right;
 
+	Node(T data) {
+		this.value = data;
+	}
+
+	@Override
+	public String toString() {
+		return "Value: " + value.toString() + " Left: " + ((left == null) ? "null" : left.value.toString()) + " Right: " + ((right == null) ? "null" : right.value.toString());
+	}
 }
--- src/com/example/exercises/binarysearchtree/Tree.java
+++ src/com/example/exercises/binarysearchtree/Tree.java
@@ -2,4 +2,46 @@ package com.example.exercises.binarysearchtree;
 
 public class Tree<T extends Comparable<T>> {
 	Node<T> root;
+
+	public Node<T> insert(Node<T> current, T data) {
+		if (root == null) {
+			root = new Node<>(data);
+			return root;
+		}
+		
+		if (current == null) {
+			return new Node<>(data);
+		} else {
+			if (data.compareTo(current.value) < 0) {
+				current.left = insert(current.left, data);
+			} else {
+				current.right = insert(current.right, data);
+			}
+			return current;
+		}
+	}
+
+	public static <T extends Comparable<T>> void printTree(Node<T> current) {
+
+		if (current != null) {
+			System.out.println(current);
+			printTree(current.left);
+			printTree(current.right);
+
+		} else {
+			return;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Tree<Integer> tree = new Tree<>();
+		tree.insert(tree.root, new Integer(13));
+		tree.insert(tree.root, new Integer(10));
+		tree.insert(tree.root, new Integer(17));
+		tree.insert(tree.root, new Integer(21));
+		tree.insert(tree.root, new Integer(15));
+		tree.insert(tree.root, new Integer(22));
+		printTree(tree.root);
+	}
 }
